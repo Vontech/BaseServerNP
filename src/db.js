708cd018181a8848923ec8b6cf604779bdbc8eec
vontell <vontell@mit.edu>
2017-12-28 20:36:14 -0500

    Use 200 'errors'

--- src/db.js
+++ src/db.js
@@ -87,7 +87,7 @@ function createUser(req, res, next) {
         
         // If the results show that a user already exists, return an error
         if (results.length > 0) {
-            res.status(409)
+            res.status(200)
             .json({
                 status: 'failure',
                 message: 'An account with this email already exists; please try another email.'
@@ -110,7 +110,7 @@ function createUser(req, res, next) {
             })
             .catch(function (err) {
                 console.log(err)
-                res.status(500)
+                res.status(200)
                 .json({
                     status: 'failure',
                     error: err.message,
